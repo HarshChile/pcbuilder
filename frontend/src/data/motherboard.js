595e4159f971ea2864210caf9a41c794bced980b
stacknaik <saurabhcode106@gmail.com>
2025-12-15 10:42:36 +0530

    Update motherboard.js

--- frontend/src/data/motherboard.js
+++ frontend/src/data/motherboard.js
@@ -1,1 +1,14 @@
-
+export const motherboard = [
+  { id: 1, src: "/images/m1.jpg", pname: "Asus B650M‑AYW WiFi Motherboard", info: "Best for basic tasks." },
+  { id: 2, src: "/images/m2.jpg", pname: "Gigabyte B550M K Motherboard", info: "Top-tier performance." },
+  { id: 3, src: "/images/m3.jpg", pname: "MSI B450M‑A Pro Max II Motherboard", info: "Flagship performance." },
+  { id: 4, src: "/images/m4.jpg", pname: "Rip Jaws R3", info: "Best for basic tasks." },
+  { id: 5, src: "/images/m5.jpg", pname: "Kingster Fury", info: "Best for basic tasks." },
+  { id: 6, src: "/images/m6.jpg", pname: "Trident JS", info: "Best for basic tasks." },
+  { id: 7, src: "/images/m7.jpg", pname: "Trident Z RGB", info: "Best for basic tasks." },
+  { id: 8, src: "/images/m8.jpg", pname: "RipJaws S5", info: "Best for basic tasks." },
+  { id: 9, src: "/images/m9.jpg", pname: "RipJaws V15", info: "Best for basic tasks." },
+  { id: 10, src: "/images/10.jpg", pname: "Intel Core i3", info: "Best for basic tasks." },
+  { id: 11, src: "/images/m11.jpg", pname: "ASkill AEGIS DDR4", info: "Best for basic tasks." },
+  { id: 12, src: "/images/m12.jpg", pname: "Kingsten Fury  S3", info: "Best for basic tasks." },
+];
